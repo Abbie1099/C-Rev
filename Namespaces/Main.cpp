81fb2966bfa3706857f3bec47beb9a980ef844b6
Abani Bhusan Das <nn.abani.pop@gmail.com>
2021-09-04 19:07:23 +0530

    Add files via upload

---
+++ Namespaces/Main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+namespace apple
+{
+	void print(const std::string &text)
+	{
+		std::cout << text << std::endl;
+	}
+}
+
+namespace orange
+{
+	void print(const char* text)
+	{
+		std::string temp = text;
+		std::reverse(temp.begin(), temp.end());
+		std::cout << temp << std::endl;
+	}
+}
+
+int main()
+{
+	apple::print("Hello");
+	orange::print("Heyyo");
+}
